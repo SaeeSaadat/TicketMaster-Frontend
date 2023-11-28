d67029d7a5e9e085781a00a174b0db490ce7bb97
darmond <bardiarezaeikalantari@gmail.com>
2023-11-28 19:54:01 +0330

    build: change dockerfiles and remove some config files for building

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import RouterPrefetch from 'vue-router-prefetch'
-import App from "./App.vue";
-import router from "./router/index";
 
+import App from "./App.vue";
+import router from "./router/routes";
 import BlackDashboard from "./plugins/blackDashboard";
+
 Vue.use(BlackDashboard);
 Vue.use(VueRouter);
 Vue.use(RouterPrefetch);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,3 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ["./src/**/*.vue", "./src/*.vue"],
 	theme: {
