04efed27fc0f015996ba2f2acdebd937b842b8c1
darmond <bardiarezaeikalantari@gmail.com>
2023-11-30 00:29:04 +0330

    refactor: remove unnecessary files

--- src/main.js
+++ src/main.js
@@ -1,16 +1,36 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import RouterPrefetch from 'vue-router-prefetch'
 
 import App from "./App.vue";
 import router from "./router/routes";
-import BlackDashboard from "./plugins/blackDashboard";
+
+import SideBar from "@/components/SidebarPlugin";
+import Notify from "@/components/NotificationPlugin";
+import {
+	BaseInput,
+	Card,
+	BaseDropdown,
+	BaseButton,
+	BaseCheckbox,
+} from "@/components/index";
+
+import "@/assets/sass/black-dashboard.scss";
+import "@/assets/css/nucleo-icons.css";
+
+Vue.component(BaseInput.name, BaseInput);
+Vue.component(Card.name, Card);
+Vue.component(BaseDropdown.name, BaseDropdown);
+Vue.component(BaseButton.name, BaseButton);
+Vue.component(BaseCheckbox.name, BaseCheckbox);
+
+Vue.use(GlobalComponents);
+Vue.use(SideBar);
+Vue.use(Notify);
 
 Vue.use(BlackDashboard);
 Vue.use(VueRouter);
-Vue.use(RouterPrefetch);
 
 new Vue({
-  router,
-  render: h => h(App)
+	router,
+	render: (h) => h(App),
 }).$mount("#app");
