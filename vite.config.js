46af2411880fae3ecdf1298c7b1df630b23ac1b2
darmond <bardiarezaeikalantari@gmail.com>
2023-12-05 00:11:05 +0330

    chore: add server configs in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,16 @@
-import { defineConfig } from 'vite'
-import { createVuePlugin as vue } from 'vite-plugin-vue2'
-import { resolve } from 'path'
+import { defineConfig } from "vite";
+import { createVuePlugin as vue } from "vite-plugin-vue2";
+import { resolve } from "path";
 
 export default defineConfig({
-
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      "@": resolve(resolve(__dirname), "src"),
-    },
-    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
-  },
-})
+	server: {
+		host: import.meta.env.VITE_HOST,
+	},
+	plugins: [vue()],
+	resolve: {
+		alias: {
+			"@": resolve(resolve(__dirname), "src"),
+		},
+		extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json", ".vue"],
+	},
+});
