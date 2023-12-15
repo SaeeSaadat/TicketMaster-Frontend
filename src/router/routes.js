05fb8d1bfaa9a2b00bd4c01094544a98c1cf274a
darmond <bardiarezaeikalantari@gmail.com>
2023-12-15 15:47:54 +0330

    docs: use comments to specify route components

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,14 +1,18 @@
+//MainPage
 const MainPage = () => import("@/pages/MainPage.vue");
 const BusinessProfile = () => import("@/pages/Profile/Business.vue");
 const UserProfile = () => import("@/pages/Profile/User.vue");
 
+//Auth
 const Login = () => import("@/pages/Auth/Login.vue");
 const Signup = () => import("@/pages/Auth/Signup.vue");
 
+//Tickets
 const Ticket = () => import("@/pages/Tickets/Ticket.vue");
 const AllBusinessTickets = () => import("@/pages/Tickets/AllBusinessTickets.vue");
 const AllUserTickets = () => import("@/pages/Tickets/AllUserTickets.vue");
 
+//Layout & Components
 const NotFound = () => import("@/components/NotFoundPage.vue");
 const DashboardLayout = () => import("@/Layout/FullPage.vue");
 
