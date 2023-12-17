b5528046d67e61e149c446265e0eb0bebe752eff
darmond <bardiarezaeikalantari@gmail.com>
2023-12-17 16:09:54 +0330

    chore: add ticketmaster text to auth pages

--- src/pages/Auth/Login.vue
+++ src/pages/Auth/Login.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="form-group w-25 mx-auto my-auto" style="background-color: #1E1D2E;">
-        <card class="py-4 px-4" style="margin-top:250px;">
+    <h1 class="mt-5 ml-3" style="font-size:80px;">TicketMaster</h1>
+        <card class="py-4 px-4" style="margin-top:100px;">
             <h5 class="title mb-5 ml-auto h1">Login</h5>
             <div class="row mb-4">
                 <customed-input customedLabelClass="text-white" customedDivClass="col-12" label="Username :" placeholder="Please Enter Your Username ..." v-model="username" />
--- src/pages/Auth/Signup.vue
+++ src/pages/Auth/Signup.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="w-25 mx-auto my-auto" style="background-color: #1E1D2E;">
-        <card class="py-4 px-4" style="margin-top:220px;">
+        <h1 class="mt-5 ml-3" style="font-size:80px;">TicketMaster</h1>
+
+        <card class="py-4 px-4" style="margin-top:100px;">
             <h5 class="title mb-5 ml-auto h1">Signup</h5>
             <div class="row mb-4">
                 <customed-input customedLabelClass="text-white" customedDivClass="col-12" label="Username :" placeholder="Please Choose a Username ..." v-model="username" />
