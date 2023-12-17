c8f309ab73ff8d65d549a187ef3794dc74757417
darmond <bardiarezaeikalantari@gmail.com>
2023-12-17 22:02:34 +0330

    feat: add icons to sidebar

--- src/Layout/Sidebar.vue
+++ src/Layout/Sidebar.vue
@@ -1,10 +1,25 @@
 <template>
     <div class="col sidebar bg-white" data="vue">
-        <a href="/dashboard/myProduct" class="btn btn-info text-white mt-5 mb-4">myProduct</a>
-        <a v-for="(value, index) in sidebarItems" class="text-h3 btn btn-simple border-0" :href="value.href">
-            <p class="h3 text-white">{{ value.innerText }}</p>
+    
+        <a href="/dashboard/myProduct" class="btn btn-simple mt-5 mb-4 w-100">
+            <div class="d-inline-flex">
+                <i class="fa fa-cart-shopping my-auto"></i>
+                <span class="my-auto ml-3">My Product</span>
+            </div>
         </a>
-        <a class="btn btn-danger mt-5" href="/">Logout</a>
+        <a v-for="(value, index) in sidebarItems" class="btn btn-simple border-0 w-100 text-left" :href="value.href">
+            <div class="d-inline-flex">
+                <i class="my-auto text-white fa" :class="value.icon"></i>
+                <span class="h4 text-white ml-2 my-auto">{{ value.innerText }}</span>
+            </div>
+        </a>
+        <p class="mb-4"></p>
+        <a class="btn btn-simple mt-5 w-100" href="/">
+            <div class="d-inline-flex">
+                <i class="fa fa-power-off my-auto"></i>
+                <span class="my-auto ml-2">Logout</span>
+            </div>
+        </a>
     </div>
 </template>
 
@@ -13,24 +28,29 @@ export default {
     data() {
         return {
             sidebarItems: [{
+                    icon: "fa-user",
                     href: "/dashboard/userProfile",
-                    innerText: "userProfile",
+                    innerText: "My Profile",
                 },
                 {
+                    icon: "fa-landmark",
                     href: "/dashboard/businessProfile",
-                    innerText: "businessProfile",
+                    innerText: "Business Profile",
                 },
                 {
+                    icon: "fa-list-check",
                     href: "/dashboard/myTickets",
-                    innerText: "myTickets",
+                    innerText: "My Tickets",
                 },
                 {
+                    icon: "fa-chart-line",
                     href: "/dashboard",
                     innerText: "Dashboard",
                 },
                 {
+                    icon: "fa-ticket",
                     href: "/dashboard/ticket",
-                    innerText: "ticket",
+                    innerText: "Ticket",
                 },
             ],
         };
--- src/pages/Auth/Login.vue
+++ src/pages/Auth/Login.vue
@@ -1,7 +1,7 @@
-<template>
-    <div class="form-group w-25 mx-auto my-auto" style="background-color: #1E1D2E;">
-    <h1 class="mt-5 ml-3" style="font-size:80px;">TicketMaster</h1>
-        <card class="py-4 px-4" style="margin-top:100px;">
+<template style="background-color: #1E1D2E;">
+    <div class="w-25 mx-auto my-auto">
+        <h1 class="mt-5 text-center mb-5" style="font-size:80px;">TicketMaster</h1>
+        <card class="py-4 px-4 mt-5">
             <h5 class="title mb-5 ml-auto h1">Login</h5>
             <div class="row mb-4">
                 <customed-input customedLabelClass="text-white" customedDivClass="col-12" label="Username :" placeholder="Please Enter Your Username ..." v-model="username" />
