dcfc93b5cf44d986b9cb8388ae457b03db7c5f37
darmond <bardiarezaeikalantari@gmail.com>
2023-12-20 11:22:13 +0330

    style: remove unnecessary styles

--- src/pages/Profile/EditUser.vue
+++ src/pages/Profile/EditUser.vue
@@ -1,14 +1,14 @@
 <template>
     <card type="user" class="mx-auto mt-4 w-50">
         <div class="row mb-3">
-            <customed-input customedLabelClass="" customedDivClass="col-12 w-50" label="User Id" placeholder="Company" :disabled="true" />
+            <customed-input customedDivClass="col-12 w-50" label="User Id" placeholder="Company" :disabled="true" />
         </div>
         <div class="row mb-3">
-            <customed-input customedLabelClass="" customedDivClass="col-6" label="Username" placeholder="Please Enter Your New Username ..." /> 
+            <customed-input customedDivClass="col-6" label="Username" placeholder="Please Enter Your New Username ..." /> 
         </div>
         <div class="row mb-5">
-            <customed-input customedLabelClass="" customedDivClass="col-6" label="Password" placeholder="Please Enter Your New Password ..." />
-            <customed-input customedLabelClass="" customedDivClass="col-6" label="Confirm Password" placeholder="Please Re-Enter Your New Password ..." />
+            <customed-input customedDivClass="col-6" label="Password" placeholder="Please Enter Your New Password ..." />
+            <customed-input customedDivClass="col-6" label="Confirm Password" placeholder="Please Re-Enter Your New Password ..." />
         </div>
         <button class="btn btn-primary">Save</button>
     </card>
