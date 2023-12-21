61b03603375ec6bbee062ec39c6dda0ce8ee4b04
darmond <bardiarezaeikalantari@gmail.com>
2023-12-21 13:31:32 +0330

    chore: add vue-mixin

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,3 @@
 <template>
     <router-view/>
 </template>
-
-<script>
-// axios.get("/user/auth").then((res)=>{
-// 	// this.$store.state.personType = res.body.type
-// 	// this.$store.state.personId = res.body.id
-// 	// this.$store.state.personUsername = res.body.username
-// }).catch(()=>{
-// 	this.$router.push("/login")
-// })
-</script>
--- src/Libraries/axios.js
+++ src/Libraries/axios.js
@@ -1,12 +1,25 @@
-import Vue from "vue"
+import Vue from "vue";
 import axios from "axios";
-
-// Vue.mixin({
-// 	beforeCreated() {
-// 		axios.interceptors.response.use("", (error) => {
-// 			if (error.response.status === 401) this.$router.push("/login");
-// 		});
-// 	},
-// });
+import Router from "@/Libraries/vue-router.js";
+import Store from "@/Libraries/store.js";
 
 axios.defaults.baseURL = import.meta.env.VITE_APP_BASE_URL;
+
+Vue.mixin({
+	beforeRouteEnter(to, from, next) {
+		next((vm) => {
+			if (to.path != "/login" && to.path != "/signup")
+				axios
+					.get("/auth/user")
+					.then((res) => {
+                        console.log(res.data);
+						// Store.state.personType = res.data.type;
+						// Store.state.personId = res.data.userId;
+						// Store.state.personUsername = res.data.username;
+					})
+					.catch(() => {
+						Router.push("/login");
+					});
+		});
+	},
+});
