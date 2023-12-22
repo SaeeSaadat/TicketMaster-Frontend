2bc223ce0a602c8b824f5c0d96fee0f5261bb419
darmond <bardiarezaeikalantari@gmail.com>
2023-12-22 14:52:26 +0330

    feat: almost complete chatbox

--- src/pages/Tickets/Ticket.vue
+++ src/pages/Tickets/Ticket.vue
@@ -1,14 +1,25 @@
 <template>
     <div>
-        <card class="w-75 mx-auto text-center p-5">
-            <h1>Title: Title</h1>
+        <card class="w-75 mx-auto text-center">
+            <h1>A Testing Ticket For ASD</h1>
             <h4>1402/09/30 18:55 By Bardia</h4>
             <h2>Status: Open/Closed/Pending/Needs Answer</h2>
             <div class="row text-center mx-auto">
                 <card class="mr-5 ml-4" style="width:30%;">Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, tempora consequatur! Tenetur vero dolorem iusto cupiditate officia omnis mollitia quo dicta vel unde praesentium debitis repellat, ab nesciunt maxime numquam?</card>
-                <card class="" style="width:60%">
+                <card style="width:60%;">
                     <p>ChatBox</p>
-                    
+                    <p class="text-left text-primary">Hello What Can i do for you?</p>
+                    <p class="text-right text-info">Hello I want to check my connection.</p>
+                    <p class="text-left text-primary">Hello What Can i do for you?</p>
+                    <p class="text-right text-info">Hello I want to check my connection.</p>
+                    <p class="text-left text-primary">Hello What Can i do for you?</p>
+                    <p class="text-right text-info">Hello I want to check my connection.</p>
+                    <p class="text-left text-primary">Hello What Can i do for you?</p>
+                    <p class="text-right text-info">Hello I want to check my connection.</p>
+                    <p class="text-left text-primary">Hello What Can i do for you?</p>
+                    <p class="text-right text-info">Hello I want to check my connection.</p>
+                    <p class="text-left text-primary">Hello What Can i do for you?</p>
+                    <p class="text-right text-info">Hello I want to check my connection.</p>
                 </card>
             </div>
         </card>
