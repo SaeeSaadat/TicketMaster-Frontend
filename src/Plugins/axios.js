b3afeea048af1e9a861c3acb242c875270207233
darmond <bardiarezaeikalantari@gmail.com>
2023-12-24 23:22:37 +0330

    feat: add axios interceptor for 401 statuscode

--- src/Plugins/axios.js
+++ src/Plugins/axios.js
@@ -1,2 +1,12 @@
 import axios from "axios";
+import Router from "./vue-router";
 axios.defaults.baseURL = import.meta.env.VITE_APP_BASE_URL;
+axios.interceptors.response.use(
+	(res) => {
+		return Promise.resolve(res);
+	},
+	(error) => {
+		if (error && error.status == 401) Router.push("/login");
+		return Promise.reject(error);
+	}
+);
