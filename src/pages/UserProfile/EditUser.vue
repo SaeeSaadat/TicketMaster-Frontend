e9ed1a92c9f4877834a2f66da1ba9c7355d9be78
darmond <bardiarezaeikalantari@gmail.com>
2024-01-01 12:28:29 +0330

    feat: integrate user profile with backend

--- src/pages/UserProfile/EditUser.vue
+++ src/pages/UserProfile/EditUser.vue
@@ -1,12 +1,15 @@
 <template>
     <card type="user" class="mx-auto mt-4 w-50">
-        <div class="row mb-3">
+        <div class="row mb-4">
             <customed-input customedDivClass="col-6 w-50" label="User Id" :placeholder="userId" :disabled="true" />
-            <customed-input customedDivClass="col-6" label="Username" :placeholder="username" />
+            <customed-input customedDivClass="col-6" label="New Username" :placeholder="username" />
+        </div>
+        <div class="row mb-4">
+            <customed-input customedDivClass="col-12" label="Old Password" placeholder="Please Enter Your Old Password ..." />
         </div>
         <div class="row mb-5">
-            <customed-input customedDivClass="col-6" label="Password" placeholder="Please Enter Your New Password ..." />
-            <customed-input customedDivClass="col-6" label="Confirm Password" placeholder="Please Re-Enter Your New Password ..." />
+            <customed-input customedDivClass="col-6" label="New Password" placeholder="Please Enter Your New Password ..." />
+            <customed-input customedDivClass="col-6" label="Confirm New Password" placeholder="Please Re-Enter Your New Password ..." />
         </div>
         <button class="btn btn-primary">Save</button>
     </card>
