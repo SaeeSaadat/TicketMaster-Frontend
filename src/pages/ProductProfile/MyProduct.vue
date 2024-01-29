afacef2e62f86afdacdd58d606669c9b160f2231
Bardia <bardiarezaeikalantari@gmail.com>
2024-01-29 16:13:51 +0330

    fix: add role to myproduct page

--- src/pages/ProductProfile/MyProduct.vue
+++ src/pages/ProductProfile/MyProduct.vue
@@ -1,11 +1,11 @@
 <template>
-    <div v-if="!role">
-        <register-product />
-    </div>
-    <div v-else>
-        <admin-product-page />
-        <all-admin-tickets />
-    </div>
+	<div v-if="!role">
+		<register-product />
+	</div>
+	<div v-else>
+		<admin-product-page />
+		<all-admin-tickets />
+	</div>
 </template>
 
 <script>
@@ -14,9 +14,15 @@ import AdminProductPage from "@/pages/ProductProfile/RegisterProduct.vue";
 import AllAdminTickets from "@/pages/Tickets/AllAdminTickets.vue";
 
 export default {
-    components: {
-        RegisterProduct,
-        AllAdminTickets
-    }
+	components: {
+		RegisterProduct,
+		AllAdminTickets,
+	},
+	computed: {
+		role() {
+			const role = localStorage.getItem("role");
+			return role == "null" ? "" : role;
+		},
+	},
 };
 </script>
